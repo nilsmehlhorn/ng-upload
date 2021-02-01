9a6d76f02bc0de1efc36a066f8b38c3461cf9cf8
mehlhorn <mail@nils-mehlhorn.de>
2021-02-01 16:41:57 +0100

    tidy up

--- apps/ui/src/app/upload.service.ts
+++ apps/ui/src/app/upload.service.ts
@@ -14,7 +14,6 @@ export class UploadService {
       .post('/api/upload', data, {
         reportProgress: true,
         observe: 'events',
-        responseType: 'blob',
       })
       .pipe(upload());
   }
